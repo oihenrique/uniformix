4740af055c692fd406e18b6a1ae45191ab243edb
Henrique <oi.henriquegomes@gmail.com>
2023-11-15 11:57:24 -0300

    add: visual table component

--- uniformix-frontend/src/app/components/components.module.ts
+++ uniformix-frontend/src/app/components/components.module.ts
@@ -4,6 +4,8 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { OrangeButtonComponent } from './orangeButton/orangeButton.component';
 import { GrayButtonComponent } from './grayButton/grayButton.component';
 import { SmallButtonComponent } from './small-button/small-button.component';
+import { TableComponent } from './table/table.component';
+import { TableInfoServiceService } from '../services/table-info-service.service';
 
 @NgModule({
   declarations: [
@@ -11,6 +13,7 @@ import { SmallButtonComponent } from './small-button/small-button.component';
     OrangeButtonComponent,
     GrayButtonComponent,
     SmallButtonComponent,
+    TableComponent,
   ],
   imports: [CommonModule],
   exports: [
@@ -18,6 +21,8 @@ import { SmallButtonComponent } from './small-button/small-button.component';
     OrangeButtonComponent,
     GrayButtonComponent,
     SmallButtonComponent,
+    TableComponent,
   ],
+  providers: [TableInfoServiceService],
 })
 export class ComponentsModule {}
