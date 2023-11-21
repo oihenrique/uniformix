80ca334b9120c5f3ab2c1bb6e8f41da3ce8bebfa
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-11-20 22:16:10 -0300

    refactor:  changing to postgres database

---
+++ api/src/main/java/edu/uniformix/api/UniformixApiApplication.java
@@ -0,0 +1,13 @@
+package edu.uniformix.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniformixApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniformixApiApplication.class, args);
+	}
+
+}
--- api/src/main/resources/db/migration/V4__create-batch-table.sql
+++ api/src/main/resources/db/migration/V4__create-batch-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE batch (
-    id BIGINT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    batch_Code VARCHAR(6) NOT NULL,
+    id SERIAL PRIMARY KEY,
+    batch_code VARCHAR(6) NOT NULL,
     description VARCHAR(255) NOT NULL,
     quantity INT NOT NULL,
-    aquisition_Date TIMESTAMP NOT NULL
+    acquisition_date TIMESTAMP NOT NULL
 );
