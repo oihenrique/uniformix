b4a831811347f6144f984e6f7da33a9dbd4b3c40
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-12-02 13:09:34 -0300

    feat: add type property to button components

--- uniformix-frontend/src/app/components/grayButton/grayButton.component.ts
+++ uniformix-frontend/src/app/components/grayButton/grayButton.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class GrayButtonComponent implements OnInit {
   @Input() buttonText: string = '';
   @Input() icon: string = '';
+  @Input() type: string = 'button';
 
   constructor() {}
 
--- uniformix-frontend/src/app/components/orangeButton/orangeButton.component.ts
+++ uniformix-frontend/src/app/components/orangeButton/orangeButton.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class OrangeButtonComponent implements OnInit {
   @Input() buttonText: string = '';
   @Input() icon: string = '';
+  @Input() type: string = 'button';
 
   constructor() {}
   ngOnInit(): void {}
