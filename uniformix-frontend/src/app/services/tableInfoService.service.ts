e587aae21857a4bda32cbadf166e4f29f5ee614a
Henrique Gomes <oi.henriquegomes@gmail.com>
2023-12-02 12:08:56 -0300

    refactor: removing unused imports

--- uniformix-frontend/src/app/services/tableInfoService.service.ts
+++ uniformix-frontend/src/app/services/tableInfoService.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { tableInfoInterface } from '../components/table/tableInfoInterface';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { UrlTree } from '@angular/router';
+
 
 @Injectable({
   providedIn: 'root',
