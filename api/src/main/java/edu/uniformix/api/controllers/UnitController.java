ca2ee1613cb865a86f40b41108862c6865c00b94
Henrique Gomes <oi.henriquegomes@gmail.com>
2024-05-09 21:04:06 -0300

    add: inativate method

--- api/src/main/java/edu/uniformix/api/controllers/UnitController.java
+++ api/src/main/java/edu/uniformix/api/controllers/UnitController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -40,5 +41,20 @@ public class UnitController {
         List<UnitListDto> unitList = unitPage.getContent();
 
         return ResponseEntity.ok(unitList);
+    }
+
+    @DeleteMapping("/{name}")
+    public ResponseEntity<Object> inativateByName(@PathVariable String name) {
+        Unit unit = unitRepository.findByName(name);
+
+        if (unit == null) {
+            return ResponseEntity.status((HttpStatus.BAD_REQUEST)).body("Unit not found");
+        }
+
+        unit.setActive(false);
+
+        unitRepository.save(unit);
+
+        return ResponseEntity.noContent().build();
     }
 }
--- api/src/main/java/edu/uniformix/api/repositories/UnitRepository.java
+++ api/src/main/java/edu/uniformix/api/repositories/UnitRepository.java
@@ -1,7 +1,12 @@
 package edu.uniformix.api.repositories;
 
+import edu.uniformix.api.domain.Batch;
 import edu.uniformix.api.domain.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UnitRepository extends JpaRepository<Unit, Long> {
+    @Query("SELECT u FROM Unit u WHERE u.name = :name")
+    Unit findByName(@Param("name") String name);
 }
