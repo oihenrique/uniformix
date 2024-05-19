6d8f148c96097c5bf8495bb5465f732984968a79
Henrique Gomes <oi.henriquegomes@gmail.com>
2024-05-19 09:04:59 -0300

    add: findByName method

--- api/src/main/java/edu/uniformix/api/repositories/UniformRepository.java
+++ api/src/main/java/edu/uniformix/api/repositories/UniformRepository.java
@@ -2,6 +2,10 @@ package edu.uniformix.api.repositories;
 
 import edu.uniformix.api.domain.Uniform;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UniformRepository extends JpaRepository<Uniform, Long> {
+    @Query("SELECT u FROM Uniform u WHERE LOWER(u.name) = LOWER(:name)")
+    Uniform findByName(@Param("name") String name);
 }
