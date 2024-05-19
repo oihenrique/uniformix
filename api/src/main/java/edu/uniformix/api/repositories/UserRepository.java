50f4ed9a4dbc2d917082290d0f798594809621c6
Henrique Gomes <oi.henriquegomes@gmail.com>
2024-05-19 09:04:02 -0300

    add: get findUserByEmail method

--- api/src/main/java/edu/uniformix/api/repositories/UserRepository.java
+++ api/src/main/java/edu/uniformix/api/repositories/UserRepository.java
@@ -5,10 +5,15 @@ import edu.uniformix.api.domain.dtos.user.UserListDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRepository extends JpaRepository<Users, String> {
     Page<UserListDto> findAllByActiveTrue(Pageable paginate);
 
     UserDetails findByLogin(String login);
+
+    @Query("SELECT e FROM Users e WHERE LOWER(e.login) LIKE %:search%")
+    Users findUserByEmail(@Param("search") String email);
 }
