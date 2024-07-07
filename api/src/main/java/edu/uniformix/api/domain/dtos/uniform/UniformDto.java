a60d9910afe9d3723ac528590cd364e43d0fa89f
Henrique Gomes <oi.henriquegomes@gmail.com>
2024-07-07 11:22:26 -0300

    fix: adding negative numbers verification on input component

--- api/src/main/java/edu/uniformix/api/domain/dtos/uniform/UniformDto.java
+++ api/src/main/java/edu/uniformix/api/domain/dtos/uniform/UniformDto.java
@@ -2,12 +2,13 @@ package edu.uniformix.api.domain.dtos.uniform;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 
 
 public record UniformDto(
         @NotBlank
         String name,
-        @NotNull
+        @NotNull @PositiveOrZero
         Integer quantity,
         String sex,
         @NotBlank
